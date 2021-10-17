d9742a12b0a7419681f17dba740834a58a360ddd
youichi Ishigaki <deepestred1968@gmail.com>
2021-10-17 18:08:54 +0900

    change

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 set -e
 
-# add container metadata to index.html
-echo "<pre>" >> /usr/local/apache2/htdocs/index.html
-curl $ECS_CONTAINER_METADATA_URI | jq '.' >> /usr/local/apache2/htdocs/index.html
-echo "</pre>" >> /usr/local/apache2/htdocs/index.html
+# add container metadata to index_1.html
+echo "<pre>" >> /usr/local/apache2/htdocs/index_1.html
+curl $ECS_CONTAINER_METADATA_URI | jq '.' >> /usr/local/apache2/htdocs/index_1.html
+echo "</pre>" >> /usr/local/apache2/htdocs/index_1.html
 
 # run httpd
 httpd-foreground
